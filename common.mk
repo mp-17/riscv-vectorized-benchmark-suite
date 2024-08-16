e1814a62660b5a59790611c419b1c1b11448ceee
Matteo Perotti <mperotti@iis.ee.ethz.ch>
2024-08-16 12:55:01 +0200

    Bring common tools outside of the folders

---
+++ common.mk
@@ -0,0 +1,7 @@
+# Common tools
+
+GCC     ?= /usr/scratch2/pisoc12/msc24f10/workspace/upstream_linux_bringup/cva6-sdk-vinc-replicate/buildroot/output/host/bin/riscv64-linux-gcc
+GXX     ?= /usr/scratch2/pisoc12/msc24f10/workspace/upstream_linux_bringup/cva6-sdk-vinc-replicate/buildroot/output/host/bin/riscv64-linux-g++
+OBJDUMP ?= /usr/scratch2/pisoc12/msc24f10/workspace/upstream_linux_bringup/cva6-sdk-vinc-replicate/buildroot/output/host/bin/riscv64-linux-objdump
+SPIKE   := /usr/scratch/tokyo/mperotti/processor/temp/rivec/deps/install/riscv-isa-sim/bin/spike
+PK      := /usr/scratch/tokyo/mperotti/processor/temp/rivec/deps/install/riscv-pk/riscv64-unknown-elf/bin/pk
